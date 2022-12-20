af01b41ae53cf8ceda19dd22cb4b7f6bf4e870ca
21hiwot <hiwotyemartalij@gmail.com>
2022-12-20 04:54:09 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/.#main.h
@@ -0,0 +1,1 @@
+root@30ef607cfcfe.1031
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ *  reset_to_98 - takes a pointet to an int parameter and updates the value it point to 98
+ *  @an: int paramrter
+ *
+ *  Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
