4c6401c121fe34e70d25e964bff5bb36dc43d12e
21hiwot <hiwotyemartalij@gmail.com>
2023-01-03 04:20:17 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * *_memset - Fills memory with a constant byte
@@ -12,6 +12,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
+
 	for (index = 0; n > 0; index++)
 	{
 		s[index] = b;
