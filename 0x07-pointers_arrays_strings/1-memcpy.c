82fca0be348c185eb7ff2686138d5103f9af5b2b
21hiwot <hiwotyemartalij@gmail.com>
2023-01-03 04:45:07 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	
-	for (index = 0; n > 0; index++)
-	{
-		dest[index] = src[index];
-		n -= 1;
-	}
-	return (dest);
+unsigned int index
+
+for (index = 0; n > 0; index++)
+{
+dest[index] = src[index];
+n -= 1;
+}
+return (dest);
 }
