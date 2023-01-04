b433fd52fb90e8d4fd19e3305e338d092572c3a9
21hiwot <hiwotyemartalij@gmail.com>
2023-01-04 15:11:40 -0800

    main.h

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,13 +8,16 @@
 * _strncat: Concatenates two strings
 */
 
-char _strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 int index = 0;
 int dest_lne = 0;
+
 while (dest[index++])
-dest_len++;
+dest_lne++;
+
 for (index = 0; src[index] && index < n; index++)
-dest[dest_len++] = src[index];
+dest[dest_lne++] = src[index];
+
 return (dest);
 }
