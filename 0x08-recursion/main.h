4d75be74c67c145156fea5806e47fc16c517b21c
21hiwot <hiwotyemartalij@gmail.com>
2023-01-04 04:43:09 -0800

    main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN.H
+#define MAIN.h
+
+/*
+ * file: main.h
+ * auth: me
+ * dece: header file containing prototypes for all functions
+ * used in the 0x0 - recurion directory.
+ */
+int _putchar(char C);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
+
