18a209f79138269650ebf0d7c0dcbbea3180f852
21hiwot <hiwotyemartalij@gmail.com>
2023-01-04 12:40:59 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,16 +6,20 @@
  * @needle: char pointer
  * @haystack: char pointer
  *
- * Return: s
- */
+ * Return: return 0
+ **/
 
 char *_strstr(char *haystack, char *needle)
 {
 	int count;
 
-	for (; haystack[0]; haystack++);
+
+	for
+	    (haystack[0] haystack++);
+
 	{
-		for (count = 0; haystack[count] == needle[count]; count++);
+
+		for (count = 0; haystack[count] == needle[count]; count++)
 			if (!(needle[count]))
 				return (haystack);
 	}
