ffc4f2b86c9be116bf7f0009823e56fd0e4babe5
21hiwot <hiwotyemartalij@gmail.com>
2023-01-04 12:26:46 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,14 +4,15 @@
  * _strpbrk -  searches a string for any of a set of bytes.
  *
  * @s: char pointer
- * 
+ *
  * @accept: char pointer
  *
- * Return: string
+ * Return: new string
  */
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int counter;
+
 	for (; *s != '\0'; s++)
 	{
 		for (counter = 0; accept[counter] != '\0'; counter++)
@@ -22,3 +23,4 @@ char *_strpbrk(char *s, char *accept)
 	}
 
 return (0);
+}
